62c07d2f7b7ac5631a921f84cf2b2cbfa8d0357a
Roope Herpiö <roope.herpio@gmail.com>
2018-06-06 21:40:21 +0300

    added new user control thing

---
+++ Assets/Scripts/Climbable.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Climbable : MonoBehaviour {
+
+     GameObject climber;
+     bool canClimb = false;
+     public float speed = 1;
+     public float top;
+     public float bottom;
+     private CharacterControl ctrl;
+ 
+    // Use this for initialization
+    void Start () {
+        //Fetch the box coordinates from the GameObject
+        BoxCollider box = GetComponent<BoxCollider>();
+        top = box.bounds.max.y;
+        bottom = box.bounds.min.y;
+    }
+        
+     void OnCollisionEnter(Collision coll)
+     {
+         if (coll.gameObject.tag == "Player")
+         {
+             canClimb = true;
+             climber = coll.gameObject;
+             ctrl = climber.GetComponent<CharacterControl>();
+         }
+     }
+ 
+     void OnCollisionExit(Collision coll2)
+     {
+         if (coll2.gameObject.tag == "Player")
+         {
+             canClimb = false;
+             climber = null;
+         }
+     }
+     void Update()
+     {
+         if (canClimb)
+         {
+             if (Input.GetAxis("Vertical")>0)
+             {
+                 climber.transform.Translate(Vector2.up * Time.deltaTime * speed);
+             }
+             if (Input.GetAxis("Vertical")<0)
+             {
+                 climber.transform.Translate(Vector2.down * Time.deltaTime * speed);
+             }
+         }
+     }
+     // function called by controller script when controller is attached to ladder
+    // delta is the forward/backward movement
+    /*
+    void Move (float delta) 
+    {
+        // we calc vertical position using current vertical position  + delta movement corrected for time interval
+        var vertPos : float = ctrl.transform.position.y + delta * Time.deltaTime;
+        ctrl.transform.rotation = transform.rotation;
+        // Debug.Log("Move: " + vertPos); // debug usefull to determine top and bottom values
+        if ((vertPos > top) || (vertPos < bottom)) 
+        {
+            // we're at top or bottom and moving away from ladder, use walk animation
+            ctrl.SendMessage("SetCurrentSpeed", delta, SendMessageOptions.DontRequireReceiver);
+            // set climb speed 0 to stop climb animation
+            ctrl.SendMessage("SetCurrentClimbSpeed", 0, SendMessageOptions.DontRequireReceiver);
+            // move forwards/backwards, orientation determined by lader orientation
+            ctrl.Move(transform.rotation * new Vector3(0.0, 0.0, delta) * Time.deltaTime);        
+        }
+        else 
+        {
+            // no forward movement as we're climbing
+            ctrl.SendMessage("SetCurrentSpeed", 0, SendMessageOptions.DontRequireReceiver);
+            // set climb animation
+            ctrl.SendMessage("SetCurrentClimbSpeed", delta, SendMessageOptions.DontRequireReceiver);
+            Vector2 pos = transform.position; // get position of ladder, use it for x and z pos of the character
+            pos.y = vertPos; // the y pos (vertical) is determined by calculated vertical position
+            ctrl.transform.position = pos;
+        }
+    }*/
+
+
+ }
--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -1,15 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerControl : MonoBehaviour
+public class PlayerControl : CharacterControl
 {
 	[HideInInspector]
 	public bool facingRight = true;			// For determining which way the player is currently facing.
 	[HideInInspector]
 
 	public bool jump = false;				// Condition for whether the player should jump.
-    private float canJumpAfter; // Timestamp when jump delay is over
-    public float jumpCoolDownInSeconds = 0.1f; // Time how long one has to wait before jumping again
+        private float canJumpAfter; // Timestamp when jump delay is over
+        public float jumpCoolDownInSeconds = 0.1f; // Time how long one has to wait before jumping again
 
 
 	public float moveForce = 365f;			// Amount of force added to move the player left and right.
@@ -17,75 +17,96 @@ public class PlayerControl : MonoBehaviour
 	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
 
 
-    public Transform groundCheck;			// A position marking the center of circle where to check if the player is grounded.
-    public bool grounded = false;           // Whether or not the player is grounded.
-    public float groundCheckRadius = 0.4f;   // Radius of ground check circle
-    public LayerMask groundLayer;
+        public Transform groundCheck;			// A position marking the center of circle where to check if the player is grounded.
+        public bool grounded = false;           // Whether or not the player is grounded.
+        public float groundCheckRadius = 0.4f;   // Radius of ground check circle, also used for vine check
+        public LayerMask groundLayer;
 
-    public Transform waterCheck;           // A position marking the center of circle where to check if the player is in water.
-    public bool watered = false;           // Whether or not the player is in water (deep enough to swim).
-    public float waterCheckRadius = 0.1f;   // Radius of water check circle
-    public LayerMask waterLayer;
-
+        public Transform waterCheck;           // A position marking the center of circle where to check if the player is in water or near vines.
+        public bool watered = false;           // Whether or not the player is in water (deep enough to swim).
+        public float waterCheckRadius = 0.1f;   // Radius of water check circle
+        public LayerMask waterLayer;
+        
+        public bool vined = false;           // Whether or not the player is near vines or other climbable things.
+        public LayerMask vineLayer;
+        public bool climbing = false;           // Whether or not the player is climbing vines or other climbable things.
+    
 	private Animator anim;					// Reference to the player's animator component.
-    private Rigidbody2D rigi;
+        private Rigidbody2D rigi;
 
 
 	void Awake()
 	{
 		// Setting up references.
-        anim = GetComponent<Animator>();
-        rigi = GetComponent<Rigidbody2D>();
-        canJumpAfter = Time.time;
+            anim = GetComponent<Animator>();
+            rigi = GetComponent<Rigidbody2D>();
+            canJumpAfter = Time.time;
 	}
 
 
 	void Update()
 	{
 		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-        grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
-        watered = Physics2D.OverlapCircle(waterCheck.position, waterCheckRadius, waterLayer);
+            grounded = Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, groundLayer);
+            watered = Physics2D.OverlapCircle(waterCheck.position, waterCheckRadius, waterLayer);
+            vined = Physics2D.OverlapCircle(waterCheck.position, groundCheckRadius, vineLayer);
 
-        float now = Time.time;
-        float vert = Input.GetAxis("Vertical");
-        // If the jump button is pressed and the player is grounded then the player should jump.
-        jump = false;
-        if (vert > 0 && grounded && now > canJumpAfter && !watered)
-        {
-            jump = true;
-            canJumpAfter = now + jumpCoolDownInSeconds;
-        }
+            float now = Time.time;
+            float vert = Input.GetAxis("Vertical");
+            float hori = Input.GetAxis("Horizontal");
+            // If the jump button is pressed and the player is grounded then the player should jump.
+            jump = false;
+            if (vert > 0 && vined && !climbing)
+            {
+                //climbing = true;
+                //rigi.velocity = new Vector2(0.0f, rigi.velocity.y);
+            }
+            else if (vert > 0 && (grounded||(climbing&&hori!=0)) && now > canJumpAfter && !watered)
+            {
+                jump = true;
+                canJumpAfter = now + jumpCoolDownInSeconds;
+                climbing = false;
+            }
+            else if (climbing&&hori!=0&&vert<0){
+                climbing = false;
+            }
+            else if (climbing&&!Physics2D.OverlapCircle(groundCheck.position, groundCheckRadius, vineLayer)){
+                climbing = false;
+            }
 	}
 
 
 	void FixedUpdate ()
 	{
-        // Cache the horizontal input.
-        float h = Input.GetAxis("Horizontal");
-        float v = Input.GetAxis("Vertical");
+            // Cache the horizontal input.
+            float h = Input.GetAxis("Horizontal");
+            float v = Input.GetAxis("Vertical");
 
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
-		anim.SetFloat("Speed", Mathf.Abs(h));
+            if(climbing){
+                h = 0;
+            }
+            // The Speed animator parameter is set to the absolute value of the horizontal input.
+            anim.SetFloat("Speed", Mathf.Abs(h));
 
-        //print(grounded);
-		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
-        if (h * rigi.velocity.x < maxSpeed)
-			// ... add a force to the player.
-			rigi.AddForce(Vector2.right * h * moveForce);
+            //print(grounded);
+            // If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
+            if (h * rigi.velocity.x < maxSpeed)
+                // ... add a force to the player.
+                rigi.AddForce(Vector2.right * h * moveForce);
 
-		// If the player's horizontal velocity is greater than the maxSpeed...
-		if(Mathf.Abs(rigi.velocity.x) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-            rigi.velocity = new Vector2(Mathf.Sign(rigi.velocity.x) * maxSpeed, rigi.velocity.y);
+            // If the player's horizontal velocity is greater than the maxSpeed...
+            if(Mathf.Abs(rigi.velocity.x) > maxSpeed)
+                // ... set the player's velocity to the maxSpeed in the x axis.
+                rigi.velocity = new Vector2(Mathf.Sign(rigi.velocity.x) * maxSpeed, rigi.velocity.y);
 
-		// If the input is moving the player right and the player is facing left...
-		if(h > 0 && !facingRight)
-			// ... flip the player.
-			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
-		else if(h < 0 && facingRight)
-			// ... flip the player.
-			Flip();
+            // If the input is moving the player right and the player is facing left...
+            if(h > 0 && !facingRight)
+                // ... flip the player.
+                Flip();
+            // Otherwise if the input is moving the player left and the player is facing right...
+            else if(h < 0 && facingRight)
+                // ... flip the player.
+                Flip();
 
 		// If the player should jump...
 		if(jump)
@@ -98,17 +119,21 @@ public class PlayerControl : MonoBehaviour
 			//AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
 
 			// Add a vertical force to the player.
-            rigi.AddForce(new Vector2(0f, jumpForce));
-            //rigi.velocity = new Vector2(rigi.velocity.x, jumpForce*0.1f);
+                        rigi.AddForce(new Vector2(0f, jumpForce));
+                        //rigi.velocity = new Vector2(rigi.velocity.x, jumpForce*0.1f);
 
 			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
 			jump = false;
 		}
-        else if (watered){
+        else if (watered || climbing){
             rigi.AddForce(Vector2.up * v * moveForce);
             if(Mathf.Abs(rigi.velocity.y) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
-            rigi.velocity = new Vector2(rigi.velocity.x, Mathf.Sign(rigi.velocity.y) * maxSpeed);
+                // ... set the player's velocity to the maxSpeed in the x axis.
+                rigi.velocity = new Vector2(rigi.velocity.x, Mathf.Sign(rigi.velocity.y) * maxSpeed);
+        }
+        if (climbing && rigi.velocity.y < 0 && v == 0)
+        {
+            rigi.velocity = new Vector2(rigi.velocity.x, 0.0f);
         }
 	}
 	
