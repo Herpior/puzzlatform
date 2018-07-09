a07e864ee888c316c3f42b36b1460c4a2a257ce2
Herpiö Roope <roope.herpio@gmail.com>
2018-07-09 15:57:29 +0300

    character control using divided control

--- Assets/Scripts/Platformer2DUserControl.cs
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -21,7 +21,7 @@ namespace UnityStandardAssets._2D
             if (!m_Jump)
             {
                 // Read the jump input in Update so button presses aren't missed.
-				m_Jump = Input.GetButtonDown("Jump");
+				m_Jump = Input.GetAxis("Vertical") > 0;//Input.GetButtonDown("Jump");
             }
         }
 
